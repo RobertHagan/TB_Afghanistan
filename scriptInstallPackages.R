414075d5c69caff1fb5026ba1183b2bb0120397a
Robert Hagan <RobertHagan@gmail.com>
2015-05-22 23:21:57 -0400

    updates Friday 22 May evenng.  with new files

---
+++ scriptInstallPackages.R
@@ -0,0 +1,164 @@
+# script to download essential files
+#saved from ubuntu
+#https://plus.google.com/u/0/+karimmezhoud_tn/posts/G9JLRnuwwem?cfem=1
+
+install.packages("glmmet")
+#ERROR: dependencies ‘GSEABase’, ‘GSEAlm’, ‘geNetClassifier’, ‘Biobase’, ‘phenoTest’ are not available for package ‘canceR’
+#* removing ‘/home/robert/R/x86_64-pc-linux-gnu-library/3.2/canceR’
+#Error: Command failed (1)
+# try to use CHAMELEON for clustering.. very robust and high qualith
+install.packages("rio")
+install.packages("RCurl") ########## not working
+install.packages("readr") ############# not working
+install.packages("RMySQL")  ############### nt working
+#library(xkcd)
+install.packages("zoo")
+install.packages("xlsx")
+install.packages("choroplethr")
+install.packages("metricsgraphics") # not working check GitHub hrbrmstr/metricsgraphics
+
+install.packages("rga")
+install.packages("fitbitScrapper")
+install.packages("XML")
+install.packages("RColorBrewer")
+install.packages("sqldf")
+install.packages("lubridate")
+install.packages("readxl")
+install.packages("readr")
+install.packages("rvest")
+install.packages("dplyr")
+install.packages("plyr")
+install.packages("reshape2")
+install.packages("stringr")
+install.packages("ggplot2")
+install.packages("quantmod")
+install.packages("googlesheets") # not working
+install.packages("jsonlite")
+install.packages("dygraphs")
+install.packages("listviewer") # check GitHub timelyportfolio/listviewer
+install.packages("googleVis")
+install.packages("tmap")
+install.packages("data.table")
+install.packages("plotly") # not working
+install.packages("DT") # GitHub rstudio/DT
+install.packages("leaflet")
+install.packages("shiny")
+install.packages("editR")
+install.packages("psych")
+install.packages("knitr")
+
+#vignette("xkcd-intro")for hand drawn chart look
+
+install.packages("ellipse")
+install.packages("corrplot")
+install.packages("caretEnsemble")
+install.packages("Epi")
+install.packages("caTools")
+install.packages(c("mlbench","pROC")) # for caretEnsemble
+install.packages("extrafont")
+install.packages("xkcd")
+install.packages("mlr")
+install.packages("gamlss")
+install.packages("StreamMetabolism") # sunrise and sunset
+#install.packages("sunrise.set")
+install.packages("MCMCpack")
+install.packages("ISLR")
+install.packages("robust")
+install.packages("useful")
+install.packages("GISTools")
+install.packages("classInt")
+install.packages("SQDA")
+install.packages("RQDA")
+install.packages("gamlss.dist")
+library(gamlss)
+gamlss(gamlss.demo)
+gamlss(demo)
+install.packages("GGally")
+install.packages("rgeos")
+install.packages("apcluster")
+install.packages("Rbbg")
+install.packages("ctv")
+
+install.packages("rgdal")
+library(ctv)
+install.views("Spatial")
+install.packages("ggthemes")
+#ls('package:GGally')
+install.packages("curl")
+install.packages("RCurl")
+
+install.packages("XML")
+install.packages("raster")
+install.packages("jsonlite") #vailable??
+install.packages("timeDate")
+install.packages("Zelig")
+install.packages("MCMCpack")#, "coda")
+install.packages("forecast")
+install.packages("igraph)")
+install.packages("lattice")
+install.packages(c("car", "mgcv","lme4", "nime", "multcomp", "vcd", "glmnet", 
+                   "survival", "caret", "shiny", "knitr", "xtable", "slidify",
+                   "sp", "maptools", "maps", "ggmap", "zoo", "ggplot2", "rgl",
+                   "googleVis", "stringr", "lubridate", "plyr", "reshape2",
+                   "reshape", "xts", "quantmod", "Rcpp", "XML", "jsonlite",
+                   "httr", "devtools", "roxygen2", "sjPlot"))
+install.packages(c("RODBC", "RMySQL", "RPostgresSQL","RSQLite"))
+install.packages("arima.sim")
+install.packages("struccharge")  # to test for constant parameters mean, sd, corr, cov
+#  can also use rolling estimates using zoo package and rollapply function
+install.packages("boot")
+install.packages("foreign")
+install.packages("xlsx")
+install.packages("dplyr")
+install.packages("reshape2")
+install.packages("GGally")
+install.packages("vcd")
+
+install.packages(c("acf","tseries","PerformanceAnalytics","zoo","rgdal", "gstat", "gridSVG"))
+install.packages(c("labeling","mvtnorm","visualizationTools","dplyr","quadprog","classInt"))
+
+installed.packages("git2r")
+
+install.packages("devtools") # problems.. does not install.
+library(devtools)
+devtools::install_github("hrbrmstr/metricsgraphics")
+install.packages("dygraphs") # http://rstudio.github.io/dygraphs/index.html
+
+install.packages("rJava")
+install_github("kmezhoud/canceR") # nice circular plots
+devtools::install_github("timelyportfolio/listviewer")
+install.packages("Rsitecatalyst") # GitHub randyzwitch/RSiteCatalyst
+devtools::install_github("randyzwitch/RsiteCatalyst")
+
+
+
+
+
+
+
+install_github("gforge/Grmd")
+install_github("ropensci/plotly")
+install_github("broom", "dgrtwo")  # for tidy, glance and augment functions
+#to use tidy(lmfit) on lmfit = lm(mpg ~ wt, mtcars)
+install.packages("rattle", repos="http://rattle.togaware.com", type="source")  #see notes 
+install.packages("wsrf", repos="http://rattle.togaware.com", type="source")
+install.packages("wsrpart", repos="http://rattle.togaware.com", type="source")
+install.packages("wskm", repos="http://rattle.togaware.com", type="source")
+install.packages("RGtk2")
+install.packages(c("xts", "hexbin", "rattle", "swirl"))
+install.packages(c("tm","Hmisc","caroline","textcat"))
+install.packages("qdap")  # Installed lots of text procesing packages and help.
+# qdap not installed
+install.packages(c("BH", "caret", "mtvnorm", "TH.data"))
+install.packages(c("textir","tau","RTextTools","rgeos"))
+install.packages("foreign")
+install.packages("RgoogleMaps")
+install.packages("mapproj")
+library(swirl)
+swirl()
+library(mvtnorm)
+
+# pmvnorm(lower=c(-1))
+install.packages("fortunes")
+library("fortunes")
+fortune()
---
+++ visNetwork.R
@@ -0,0 +1,133 @@
+#visNetwork
+#https://plus.google.com/u/0/communities/117681470673972651781?cfem=1
+
+#http://dataknowledge.github.io/visNetwork/
+#devtools::install_github("DataKnowledge/visNetwork")
+#visNetwork
+#visNetwork is a R package for network visualization,
+#using vis.js javascript library (http://visjs.org/).
+#All the remarks and bugs returns are welcome on
+#github : https://github.com/DataKnowledge/visNetwork.
+
+#Installation
+#Package is actually only available on github. You can install it with :
+  
+#  devtools::install_github("DataKnowledge/visNetwork")
+
+
+
+require(visNetwork, quietly = TRUE)
+# minimal example
+nodes <- data.frame(id = 1:3)
+edges <- data.frame(from = c(1,2), to = c(1,3))
+visNetwork(nodes, edges, width = "100%")
+#
+#More variables
+#You can add informations and customize network simply using more variables
+#on nodes and edges data.frames. For example :
+  
+#  label column : add labels on nodes / edges
+#value column : control size on nodes / edges
+#title column : add tooltip on nodes / edges (html, or character)
+#group column : Only for nodes. Add nodes groups.
+#…
+nb <- 10
+nodes <- data.frame(id = 1:nb, label = paste("Label", 1:nb),
+                    group = sample(LETTERS[1:3], nb, replace = TRUE), value = 1:nb,
+                    title = paste0("<p>", 1:nb,"<br>Tooltip !</p>"), stringsAsFactors = FALSE)
+
+edges <- data.frame(from = trunc(runif(nb)*(nb-1))+1,
+                    to = trunc(runif(nb)*(nb-1))+1,
+                    value = rnorm(nb, 10), label = paste("Edge", 1:nb),
+                    title = paste0("<p>", 1:nb,"<br>Edge Tooltip !</p>"))
+
+visNetwork(nodes, edges, width = "100%")
+
+#Nodes / edges options
+#Several global options are available for nodes and edges using
+#visNodes and visEdges.
+
+# change shape and color of nodes
+# and style of edges
+nodes <- data.frame(id = 1:3)
+edges <- data.frame(from = c(1,2), to = c(3,3))
+visNetwork(nodes, edges) %>% 
+  visNodes(shape = "square", color = "darkblue") %>%
+  visEdges(style ="dash-line")
+
+#Groups options
+#You can use options per group using visGroups.
+
+nodes <- data.frame(id = 1:5,  
+                    group = sample(c("A", "B"), 5, replace = TRUE))
+edges <- data.frame(from = c(2,5,3), to = c(1,2,4))
+
+visNetwork(nodes, edges, width = "100%") %>%
+  visGroups(groupname = "A", color = "red", shape = "database") %>%
+  visGroups(groupname = "B", color = "yellow", shape = "label")
+
+#Network Options
+#A lot of configuration options are available in visOptions :
+  
+#  Highlight nearest
+#Highlight nearest nodes and edges clicking on a node :
+  
+visNetwork(nodes, edges, width = "100%") %>% visOptions(highlightNearest = TRUE)
+
+#Add legend on groups
+visNetwork(nodes, edges, width = "100%",  legend = TRUE)
+
+#Select by node id
+visNetwork(nodes, edges, width = "100%") %>% visOptions(highlightNearest = TRUE, nodesIdSelection = TRUE)
+
+#Directed Network
+visNetwork(nodes, edges, width = "100%") %>% visEdges(style = "arrow")
+
+#Custom navigation
+#vis.js propose some navigation tools :
+  
+ visNetwork(nodes, edges, width = "100%") %>% visOptions(navigation = TRUE)
+
+# Data Manipulation
+# And some data manipulation tools :
+   
+visNetwork(nodes, edges, width = "100%") %>% visOptions(dataManipulation = TRUE)
+
+#Hierarchical Layout
+#You can use and control hierarchical layout with visHierarchicalLayout :
+  
+  visNetwork(nodes, edges, width = "100%") %>% visEdges(style = "arrow") %>% 
+  visHierarchicalLayout(layout = "direction") 
+
+  visNetwork(nodes, edges, width = "100%") %>% visEdges(style = "arrow") %>% 
+    visHierarchicalLayout(direction = "LR", layout = "direction")
+  
+#  Freeze network
+  visNetwork(nodes, edges, width = "100%") %>% visOptions(dragNetwork = FALSE, dragNodes = FALSE)
+ 
+ # Clustering
+  nodes <- data.frame(id = 1:10)
+  edges <- data.frame(from = round(runif(12)*10), to = round(runif(12)*10))
+  
+  visNetwork(nodes, edges, width = "100%") %>%
+    visClustering(initialMaxNodes = 5, nodeScaling = list(width = 50, height = 50, radius = 50))
+  
+ # Additional features
+#  Export
+  network <- visNetwork(nodes, edges, width = "100%")
+  htmlwidgets::saveWidget(network, "network.html")
+ # Use DOT language data
+  visNetwork(dot = 'dinetwork {1 -> 1 -> 2; 2 -> 3; 2 -- 4; 2 -> 1 }', width = "100%")
+ 
+#  Use gephi json export file
+  # don't run here
+  visNetwork(gephi = 'WorldCup2014.json')
+#  Physics, tooltip, events, shiny, …
+ # You can also control :
+#    
+#    physics of network : visPhysics
+#  tootlip : visTooltips
+#  events : visEvents
+ # And use in shiny apps with visNetworkOutput and renderVisNetwork   
+  
+  
